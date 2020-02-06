e63f4dcbdd1509c98c983c3f1dd7f4f3c0aa6c29
Thomas Liennard <thomas.liennard@gmail.com>
2020-02-06 15:49:19 +0100

    wip

--- src/serial_comm_hal.hpp
+++ src/serial_comm_hal.hpp
@@ -1,12 +1,19 @@
 #include "goldo_comm/comm.hpp"
 
-class SerialCommHal
+class SerialCommHal : public goldo_comm::CommHal
 {
   public:
+    ~SerialCommHal() override;
     void open(const char* port_name, int baudrate);
-	void spinOnce();
+	
+	uint8_t* lock_read(size_t& buffer_size) override;
+    void unlock_read(size_t buffer_size) override;
+	uint8_t* lock_write(size_t& buffer_size) override;
+	void unlock_write(size_t buffer_size) override;
     
   private:
-    int m_fd;    
+    int m_fd;
+	uint8_t m_read_buffer[256];
+	uint8_t m_write_buffer[256];
     void setInterfaceAttribs(int baudrate);    
 };
